6eadda09a4f9d6f222f248c84cba668316d0610b
Mowlavi <mowlavis@student.douglascollege.ca>
2025-05-28 14:35:54 -0700

    getter and setters

--- Backend/studyspace/src/main/java/com/example/studyspace/model/Student.java
+++ Backend/studyspace/src/main/java/com/example/studyspace/model/Student.java
@@ -20,4 +20,68 @@ public class Student {
     private String level_of_studies;
 
     // Getters and Setters
+    // Getters and Setters
+    public Long getSID() {
+        return SID;
+    }
+
+    public void setSID(Long SID) {
+        this.SID = SID;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public Date getDOB() {
+        return DOB;
+    }
+
+    public void setDOB(Date DOB) {
+        this.DOB = DOB;
+    }
+
+    public String getLevel_of_studies() {
+        return level_of_studies;
+    }
+
+    public void setLevel_of_studies(String level_of_studies) {
+        this.level_of_studies = level_of_studies;
+    }
 }
