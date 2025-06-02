a9b49c51d14a39700474532c4b5e8d480e16ff0e
Porrangi <porrangiy@student.douglascollege.ca>
2025-06-02 16:22:17 -0700

    added cross origins

--- Backend/studyspace/src/main/java/com/example/studyspace/controller/StudentCourseController.java
+++ Backend/studyspace/src/main/java/com/example/studyspace/controller/StudentCourseController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/student-courses")
+@CrossOrigin(origins = "http://localhost:3000")
 public class StudentCourseController {
 
     @Autowired
--- Backend/studyspace/src/main/java/com/example/studyspace/controller/TaskCourseController.java
+++ Backend/studyspace/src/main/java/com/example/studyspace/controller/TaskCourseController.java
@@ -9,6 +9,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/task-courses")
+@CrossOrigin(origins = "http://localhost:3000")
 public class TaskCourseController {
 
     @Autowired
